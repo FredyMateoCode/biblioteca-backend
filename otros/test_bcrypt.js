be57fc13fd9674b2069f3f4f47b7e3c597bb0aa3
BibliotecaCod <divinapastorabiblioteca@gmail.com>
2025-09-15 10:36:49 -0500

    Prueba de Backend desplegaod

--- otros/test_bcrypt.js
+++ otros/test_bcrypt.js
@@ -1,6 +1,6 @@
 const bcrypt = require('bcryptjs');
 
-const password = '0000';
+const password = '3107';
 const saltRounds = 10; // Un número estándar para la sal
 
 bcrypt.hash(password, saltRounds, function(err, hash) {
